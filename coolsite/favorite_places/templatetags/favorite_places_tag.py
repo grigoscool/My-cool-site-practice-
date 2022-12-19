9e7268be8c62951790c42d4b60a81aec11c977ec
Sergey Grigorev <grigoscool@mail.ru>
2022-12-19 19:00:42 +0300

    v3.5 user incluging tag add

--- coolsite/favorite_places/templatetags/favorite_places_tag.py
+++ coolsite/favorite_places/templatetags/favorite_places_tag.py
@@ -7,3 +7,8 @@ register = template.Library()
 @register.simple_tag()
 def get_people():
     return People.objects.all()
+
+@register.inclusion_tag('list_people.html')
+def show_people():
+    people = People.objects.all()
+    return {'people':people}
